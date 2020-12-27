58b4b27c228702d4e0d071313ac5ad5dc27a66ae
aZapata <jaalza@gmail.com>
2020-12-27 18:08:33 -0500

    Update crowdfundInstance.js

--- contracts/crowdfundInstance.js
+++ contracts/crowdfundInstance.js
@@ -1,85 +1,85 @@
 /* eslint-disable */
 import web3 from './web3';
 
-const address = '0x9fc1a2e85b3ae6b4b0a36146929f2c1a860689af';
+const address = '0xd9145CCE52D386f254917e481eB44e9943F39138';
 const abi = [
-    {
-        "anonymous": false,
-        "inputs": [
-            {
-                "indexed": false,
-                "name": "contractAddress",
-                "type": "address"
-            },
-            {
-                "indexed": false,
-                "name": "projectStarter",
-                "type": "address"
-            },
-            {
-                "indexed": false,
-                "name": "projectTitle",
-                "type": "string"
-            },
-            {
-                "indexed": false,
-                "name": "projectDesc",
-                "type": "string"
-            },
-            {
-                "indexed": false,
-                "name": "deadline",
-                "type": "uint256"
-            },
-            {
-                "indexed": false,
-                "name": "goalAmount",
-                "type": "uint256"
-            }
-        ],
-        "name": "ProjectStarted",
-        "type": "event"
-    },
-    {
-        "constant": false,
-        "inputs": [
-            {
-                "name": "title",
-                "type": "string"
-            },
-            {
-                "name": "description",
-                "type": "string"
-            },
-            {
-                "name": "durationInDays",
-                "type": "uint256"
-            },
-            {
-                "name": "amountToRaise",
-                "type": "uint256"
-            }
-        ],
-        "name": "startProject",
-        "outputs": [],
-        "payable": false,
-        "stateMutability": "nonpayable",
-        "type": "function"
-    },
-    {
-        "constant": true,
-        "inputs": [],
-        "name": "returnAllProjects",
-        "outputs": [
-            {
-                "name": "",
-                "type": "address[]"
-            }
-        ],
-        "payable": false,
-        "stateMutability": "view",
-        "type": "function"
-    }
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "returnAllProjects",
+		"outputs": [
+			{
+				"name": "",
+				"type": "address[]"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "title",
+				"type": "string"
+			},
+			{
+				"name": "description",
+				"type": "string"
+			},
+			{
+				"name": "durationInDays",
+				"type": "uint256"
+			},
+			{
+				"name": "amountToRaise",
+				"type": "uint256"
+			}
+		],
+		"name": "startProject",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"name": "contractAddress",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"name": "projectStarter",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"name": "projectTitle",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"name": "projectDesc",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"name": "deadline",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"name": "goalAmount",
+				"type": "uint256"
+			}
+		],
+		"name": "ProjectStarted",
+		"type": "event"
+	}
 ];
 
 const instance = new web3.eth.Contract(abi, address);
