e7af577c13dab4cc954b7758bfab7b4e7e8ca461
aZapata <jaalza@gmail.com>
2020-12-28 09:20:08 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -255,11 +255,35 @@ export default {
     },
     fundProject(address) {
       // Operations for funding an existing crowdfunding project will be here!
-      console.log('Fund project!');
+        cif (!this.projectData[index].fundAmount) {
+          return;
+        }
+
+        const projectContract = this.projectData[index].contract;
+        this.projectData[index].isLoading = true;
+        projectContract.methods.contribute().send({
+          from: this.account,
+          value: web3.utils.toWei(this.projectData[index].fundAmount, 'ether'),
+        }).then((res) => {
+          const newTotal = parseInt(res.events.FundingReceived.returnValues.currentTotal, 10);
+          const projectGoal = parseInt(this.projectData[index].goalAmount, 10);
+          this.projectData[index].currentAmount = newTotal;
+          this.projectData[index].isLoading = false;
+
+          // Set project state to success
+          if (newTotal >= projectGoal) {
+            this.projectData[index].currentState = 2;
+          }
+        });
     },
     getRefund(address) {
       // Operations for getting refund (funded amount to an already expired project)
-      console.log('Get refund!');
+      this.projectData[index].isLoading = true;
+      this.projectData[index].contract.methods.getRefund().send({
+        from: this.account,
+      }).then(() => {
+        this.projectData[index].isLoading = false;
+      });
     },
   },
 };
