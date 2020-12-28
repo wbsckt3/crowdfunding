1f2b2425162e0f76afff1b1d083d73548ccc8ce5
aZapata <jaalza@gmail.com>
2020-12-28 13:20:23 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
         >
           <v-flex mb-4>
             <h1 class="display-2 font-weight-bold mb-3">
-              Crowdfunding
+              Crowdfunding 
             </h1>
             <p class="subheading font-weight-regular">
               Utilizing Ethereum for Decentralized Crowdfunding
