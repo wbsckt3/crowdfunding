1b98e25aed64120d0998ff0f8da9e57b0c1f94b6
aZapata <jaalza@gmail.com>
2020-12-28 09:01:25 -0500

    Update crowdfundInstance.js

--- contracts/crowdfundInstance.js
+++ contracts/crowdfundInstance.js
@@ -1,11 +1,89 @@
 /* eslint-disable */
 import web3 from './web3';
 
-const address = ''; // Your deployed contract's address goes here
+const address = '0xd9145CCE52D386f254917e481eB44e9943F39138'; // Your deployed contract's address goes here
 // Example:
 // const address = '0x09r80cnasjfaks93m9v2';
 
-const abi = []; // Your ABI goes here (Crowdfunding contract)
+const abi = [
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "returnAllProjects",
+		"outputs": [
+			{
+				"name": "",
+				"type": "address[]"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "title",
+				"type": "string"
+			},
+			{
+				"name": "description",
+				"type": "string"
+			},
+			{
+				"name": "durationInDays",
+				"type": "uint256"
+			},
+			{
+				"name": "amountToRaise",
+				"type": "uint256"
+			}
+		],
+		"name": "startProject",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"name": "contractAddress",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"name": "projectStarter",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"name": "projectTitle",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"name": "projectDesc",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"name": "deadline",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"name": "goalAmount",
+				"type": "uint256"
+			}
+		],
+		"name": "ProjectStarted",
+		"type": "event"
+	}
+]; // Your ABI goes here (Crowdfunding contract)
 // Example:
 // const abi = [
 //     {
