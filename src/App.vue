edc4ccb0241a3bacde17f54f061397bced8ed653
aZapata <jaalza@gmail.com>
2020-12-28 13:47:53 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,11 @@
           wrap
         >
           <v-flex mb-4>
-            <h2 class="display-2 font-weight-bold mb-3">
+            <h2 class="">
                EthProxy 🌱 | Crowdfunding Colombia 
             </h2>
             <p class="subheading font-weight-regular">
-              Utiliza Ethereum para crowdfunding descentralizado 
+              Utiliza Ethereum para crowdfunding descentralizado<br> 
               Adquiere algo de Ether para realizar tu aporte o crear tu propio proyecto instalando
               <a href="https://metamask.io" target="_blank">Metamask (Extension Google Chrome)</a>
             </p>
