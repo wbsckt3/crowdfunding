cb04ffa5904212ce7defeda23371ab01f8017e7b
aZapata <jaalza@gmail.com>
2020-12-28 09:31:38 -0500

    Update crowdfundProjectInstance.js

--- contracts/crowdfundProjectInstance.js
+++ contracts/crowdfundProjectInstance.js
@@ -5,11 +5,100 @@ const abi = [
 	{
 		"constant": true,
 		"inputs": [],
-		"name": "returnAllProjects",
+		"name": "creator",
 		"outputs": [
 			{
 				"name": "",
-				"type": "address[]"
+				"type": "address"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "amountGoal",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "raiseBy",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "contributions",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "title",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "completeAt",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "description",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string"
 			}
 		],
 		"payable": false,
@@ -18,65 +107,161 @@ const abi = [
 	},
 	{
 		"constant": false,
-		"inputs": [
-			{
-				"name": "title",
-				"type": "string"
-			},
-			{
-				"name": "description",
-				"type": "string"
-			},
-			{
-				"name": "durationInDays",
-				"type": "uint256"
-			},
-			{
-				"name": "amountToRaise",
-				"type": "uint256"
-			}
-		],
-		"name": "startProject",
+		"inputs": [],
+		"name": "checkIfFundingCompleteOrExpired",
 		"outputs": [],
 		"payable": false,
 		"stateMutability": "nonpayable",
 		"type": "function"
 	},
 	{
-		"anonymous": false,
-		"inputs": [
+		"constant": false,
+		"inputs": [],
+		"name": "getRefund",
+		"outputs": [
 			{
-				"indexed": false,
-				"name": "contractAddress",
-				"type": "address"
-			},
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "state",
+		"outputs": [
 			{
-				"indexed": false,
+				"name": "",
+				"type": "uint8"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "currentBalance",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [],
+		"name": "contribute",
+		"outputs": [],
+		"payable": true,
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getDetails",
+		"outputs": [
+			{
 				"name": "projectStarter",
 				"type": "address"
 			},
 			{
-				"indexed": false,
 				"name": "projectTitle",
 				"type": "string"
 			},
 			{
-				"indexed": false,
 				"name": "projectDesc",
 				"type": "string"
 			},
 			{
-				"indexed": false,
 				"name": "deadline",
 				"type": "uint256"
 			},
 			{
-				"indexed": false,
+				"name": "currentState",
+				"type": "uint8"
+			},
+			{
+				"name": "currentAmount",
+				"type": "uint256"
+			},
+			{
 				"name": "goalAmount",
 				"type": "uint256"
 			}
 		],
-		"name": "ProjectStarted",
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"name": "projectStarter",
+				"type": "address"
+			},
+			{
+				"name": "projectTitle",
+				"type": "string"
+			},
+			{
+				"name": "projectDesc",
+				"type": "string"
+			},
+			{
+				"name": "fundRaisingDeadline",
+				"type": "uint256"
+			},
+			{
+				"name": "goalAmount",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"name": "contributor",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"name": "amount",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"name": "currentTotal",
+				"type": "uint256"
+			}
+		],
+		"name": "FundingReceived",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"name": "recipient",
+				"type": "address"
+			}
+		],
+		"name": "CreatorPaid",
 		"type": "event"
 	}
 ];
