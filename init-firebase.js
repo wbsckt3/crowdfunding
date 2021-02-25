b49be7eff4e09746c128a7e42df809fdbd55f97e
aZapata <jaalza@gmail.com>
2021-02-25 16:22:03 -0500

    Create init-firebase.js

---
+++ init-firebase.js
@@ -0,0 +1,13 @@
+// TODO: Replace the following with your app's Firebase project configuration
+var firebaseConfig = {
+  apiKey: " AIzaSyAmsXyiUoY6XhAb6L8TkykUIfsanuvXKOA",
+  authDomain: "https://maxsat.firebaseio.com",
+  databaseURL: "https://maxsat.firebaseio.com",
+  projectId: "firebase-maxsat",
+  storageBucket: "",
+  messagingSenderId: "",
+  appID: "",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
