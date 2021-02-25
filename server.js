19ac868a2fefdfd76ccd206d54e06c0aa10bc0b4
aZapata <jaalza@gmail.com>
2021-02-25 16:21:25 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
 app = express();
+
+app.get('/login', function (req, res) {
+  res.sendFile(__dirname + '/fauth.html');
+ });
+ 
+ app.get('/init-firebase.js', function (req, res) {
+  res.sendFile(__dirname + '/init-firebase.js');
+ });
+
 app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 80;
 app.listen(port);
