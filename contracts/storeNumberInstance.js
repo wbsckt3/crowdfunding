c3df7813971544abe4e2174be1fd9f104c7177a5
wbsckt3 <85421189+wbsckt3@users.noreply.github.com>
2021-07-08 18:52:02 -0500

    Update storeNumberInstance.js

--- contracts/storeNumberInstance.js
+++ contracts/storeNumberInstance.js
@@ -1,84 +1,33 @@
 /* eslint-disable */
 import web3 from './web3';
 
-const address = '0x8CfAc34881D1ceFc125e87EaB1Cd95d6Ca3fd789';
+const address = '0x4f221BB63440bADdD809ADF1b0380d3e16c0f2Bb';
 const abi = [
 	{
-		"constant": true,
-		"inputs": [],
-		"name": "returnAllProjects",
-		"outputs": [
-			{
-				"name": "",
-				"type": "address[]"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"constant": false,
 		"inputs": [
 			{
-				"name": "title",
-				"type": "string"
-			},
-			{
-				"name": "description",
-				"type": "string"
-			},
-			{
-				"name": "durationInDays",
-				"type": "uint256"
-			},
-			{
-				"name": "amountToRaise",
+				"internalType": "uint256",
+				"name": "_coolNumber",
 				"type": "uint256"
 			}
 		],
-		"name": "startProject",
+		"name": "setCoolNumber",
 		"outputs": [],
-		"payable": false,
 		"stateMutability": "nonpayable",
 		"type": "function"
 	},
 	{
-		"anonymous": false,
-		"inputs": [
+		"inputs": [],
+		"name": "coolNumber",
+		"outputs": [
 			{
-				"indexed": false,
-				"name": "contractAddress",
-				"type": "address"
-			},
-			{
-				"indexed": false,
-				"name": "projectStarter",
-				"type": "address"
-			},
-			{
-				"indexed": false,
-				"name": "projectTitle",
-				"type": "string"
-			},
-			{
-				"indexed": false,
-				"name": "projectDesc",
-				"type": "string"
-			},
-			{
-				"indexed": false,
-				"name": "deadline",
-				"type": "uint256"
-			},
-			{
-				"indexed": false,
-				"name": "goalAmount",
+				"internalType": "uint256",
+				"name": "",
 				"type": "uint256"
 			}
 		],
-		"name": "ProjectStarted",
-		"type": "event"
+		"stateMutability": "view",
+		"type": "function"
 	}
 ];
 
